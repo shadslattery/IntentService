441772e22d524dc16d4ebe5200184fe16a77d0ef
Olay G <olayg@icloud.com>
2019-10-10 10:43:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -0,0 +1,9 @@
+package com.example.android.background.sync;
+
+// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+
+//  TODO (10) Create a default constructor that calls super with the name of this class
+
+//  TODO (11) Override onHandleIntent
+//      TODO (12) Get the action from the Intent that started this Service
+//      TODO (13) Call ReminderTasks.executeTask and pass in the action to be performed
